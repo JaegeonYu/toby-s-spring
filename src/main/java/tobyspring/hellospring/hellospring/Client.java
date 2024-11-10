5732c397c39403fa5152cdb300d403dd8eab9a04
유재건 <yjk9805@naver.com>
2024-11-10 10:07:59 +0900

    feat: 스프링 컨테이너와 의존관계 주입

--- src/main/java/tobyspring/hellospring/hellospring/Client.java
+++ src/main/java/tobyspring/hellospring/hellospring/Client.java
@@ -1,6 +1,8 @@
 package tobyspring.hellospring.hellospring;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.io.IOException;
 import java.math.BigDecimal;
@@ -10,8 +12,9 @@ public class Client {
 //        PaymentService paymentService = new WebApiExRatePaymentService();
 
 
-        ObjectFactory objectFactory = new ObjectFactory();
-        PaymentService paymentService = objectFactory.paymentService();
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
+        PaymentService paymentService = beanFactory.getBean(PaymentService.class);
+
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }

f339314d4928b9ba31fce6e472f375ed753605d7
유재건 <yjk9805@naver.com>
2024-11-24 14:52:24 +0900

    feat: DataAcessException과 예외 추상화

--- src/main/java/tobyspring/hellospring/hellospring/DataClient.java
+++ src/main/java/tobyspring/hellospring/hellospring/DataClient.java
@@ -5,6 +5,9 @@ import jakarta.persistence.EntityManagerFactory;
 import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 import tobyspring.hellospring.hellospring.data.OrderRepository;
 import tobyspring.hellospring.hellospring.order.Order;
 
@@ -15,12 +18,24 @@ public class DataClient {
     public static void main(String[] args) throws IOException {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
         OrderRepository orderRepository = beanFactory.getBean(OrderRepository.class);
+        JpaTransactionManager transactionManager = beanFactory.getBean(JpaTransactionManager.class);
+        try{
+            new TransactionTemplate(transactionManager).execute(status -> {
+                Order order = new Order("100", BigDecimal.TEN);
+                orderRepository.save(order);
 
-        Order order = new Order("100", BigDecimal.TEN);
-        orderRepository.save(order);
+                Order order2 = new Order("100", BigDecimal.ONE);
+                orderRepository.save(order2);
 
-        Order order2 = new Order("100", BigDecimal.ONE);
-        orderRepository.save(order2);
+                return null;
+            });
+        }catch (DataIntegrityViolationException e){
+            System.out.println("주문번호 중복 복구 작업");
+        }
+
+
+
+
 
     }
 }

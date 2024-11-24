c15e254b80d365473379fd787b9ba8e540a88434
유재건 <yjk9805@naver.com>
2024-11-24 14:19:02 +0900

    feat: OrderRepository와 예외

--- src/main/java/tobyspring/hellospring/hellospring/DataClient.java
+++ src/main/java/tobyspring/hellospring/hellospring/DataClient.java
@@ -2,8 +2,10 @@ package tobyspring.hellospring.hellospring;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import tobyspring.hellospring.hellospring.data.OrderRepository;
 import tobyspring.hellospring.hellospring.order.Order;
 
 import java.io.IOException;
@@ -12,21 +14,13 @@ import java.math.BigDecimal;
 public class DataClient {
     public static void main(String[] args) throws IOException {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
-        EntityManagerFactory emf = beanFactory.getBean(EntityManagerFactory.class);
-
-        EntityManager em = emf.createEntityManager();
-
-        em.getTransaction().begin();
+        OrderRepository orderRepository = beanFactory.getBean(OrderRepository.class);
 
         Order order = new Order("100", BigDecimal.TEN);
-        em.persist(order);
+        orderRepository.save(order);
 
-        System.out.println(order);
-
-        em.getTransaction().commit();
-        em.close();
-
-
+        Order order2 = new Order("100", BigDecimal.ONE);
+        orderRepository.save(order2);
 
     }
 }
